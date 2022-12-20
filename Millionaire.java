bba58808daf6820ec0ff9ecc3407d8fe0c29a4a6
TheLearnedDoctor <103543085+TheLearnedDoctor@users.noreply.github.com>
2022-12-20 18:38:51 -0500

    Update and rename Millionaire. to Millionaire.java

---
+++ Millionaire.java
@@ -0,0 +1,13 @@
+public class Millionaire {
+
+	public static void main(String[] args) {
+		double amountInAccount;
+			
+		amountInAccount = 50.22;
+		amountInAccount = amountInAccount = 1000000;
+		
+		System.out.print("you have $");
+		System.out.print(amountInAccount);
+		System.out.println(" in your account.");
+	}
+}
