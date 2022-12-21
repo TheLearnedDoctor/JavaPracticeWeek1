214b0a00011aaaba7cda4f175880cb37136ad6e0
TheLearnedDoctor <103543085+TheLearnedDoctor@users.noreply.github.com>
2022-12-20 19:07:39 -0500

    Create Anniversary.java

---
+++ Anniversary.java
@@ -0,0 +1,13 @@
+public class Anniversary {
+  
+  public static void main(String[] args) {
+    int numberOfYears;
+    int numberOfAnniversaries;
+    
+    numberOfYears = 8;
+    numberOfAnniversaries = numberOfYears / 4;
+    
+    System.out.print("Number of anniversaries: ");
+    System.out.println(numberOfAnniversaries);
+  }
+}
