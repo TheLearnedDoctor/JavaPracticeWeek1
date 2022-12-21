c0b67b48055020ac324a9535655fc9a0866e133b
TheLearnedDoctor <103543085+TheLearnedDoctor@users.noreply.github.com>
2022-12-20 19:23:20 -0500

    Create CharDemo.java

---
+++ CharDemo.java
@@ -0,0 +1,7 @@
+public class CharDemo {
+	public static void main(String[] args) {
+		char myLittleChar = 'b';
+		char myBigChar = Character.toUpperCase(myLittleChar);
+		Syste,.out.println(myBigChar);
+	}
+}
