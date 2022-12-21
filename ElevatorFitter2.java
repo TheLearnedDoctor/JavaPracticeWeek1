f327db82b75e7435da52a8a3f80cd4a14019b07c
TheLearnedDoctor <103543085+TheLearnedDoctor@users.noreply.github.com>
2022-12-20 19:29:52 -0500

    Create ElevatorFitter2.java

---
+++ ElevatorFitter2.java
@@ -0,0 +1,11 @@
+public class ElevatorFitter2 {
+  public static void main(String[] args) {
+    System.out.println("True or False?");
+    System.out.println("You can fit all ten of the Brickenchiker dectuplets on the elevator:");
+    int weightOfAPerson = 150, elevatorWeightLimit = 1400;
+    int numberOfPeople = elevatorWeightLimit / weightOfAPerson;
+    boolean allTenOkay = numberOfpeople >= 10;
+    System.out.println(allTenOkay);
+  }
+}
+    
